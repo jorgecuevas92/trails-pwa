32ca276af3be85fb139c18a9c9a001e6dcef309b
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2016-11-14 03:24:29 -0600

    Added postgresql adapter for database support

--- config/database.js
+++ config/database.js
@@ -26,7 +26,26 @@ module.exports = {
      * To use this store, uncomment the "sqlitedev" store section below, and
      * run "npm install --save waterline-sqlite3"
      */
+    postgresdb: {
+      adapter: require('waterline-postgresql'),
+      connection: {
+        database: 'trails_pwa',
+        host: 'localhost',
+        user: 'trails',
+        password: '',
+        port: 5432,
+        ssl: false
+      },
+      /**
+       * Pool configuration
+       */
+      pool: {
+        min: 2,
+        max: 20
+      },
+      migrate: 'alter',
 
+    },
 
     dev: {
       adapter: require('waterline-sqlite3'),
@@ -36,7 +55,7 @@ module.exports = {
   },
 
   models: {
-    defaultStore: 'dev',
-    migrate: 'safe'
+    defaultStore: 'postgresdb',
+    migrate: 'alter'
   }
 }
